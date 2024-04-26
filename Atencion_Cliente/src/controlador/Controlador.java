88bc7102e734cf687011136ba5a804a0a19398c4
Manudeserti <82077028+Manudeserti@users.noreply.github.com>
2024-04-26 14:52:34 -0300

    cambio de ubicacion de controlador Atencion_Cliente
    cambio de ubicacion de controlador Atencion_Cliente

--- Atencion_Cliente/src/controlador/Controlador.java
+++ Atencion_Cliente/src/controlador/Controlador.java
@@ -1,5 +1,55 @@
 package controlador;
 
-public class Controlador {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import modelo.atencion.Atencion;
+import modelo.box.Box;
+import vistas.LoginEmpleado;
+import vistas.VentanaMainEmpleado;
+
+public class Controlador implements ActionListener{
+	private LoginEmpleado vistaLoginEmpleado;
+	private VentanaMainEmpleado vistaPrincipalEmpleado;
+	private Box boxAtencion;
+	
+	public Controlador() {
+		this.vistaLoginEmpleado= new LoginEmpleado();
+		this.vistaLoginEmpleado.setActionListener(this);
+		
+	}
+ 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getActionCommand().equals("INGRESAR")) {
+			String numeroBox = this.vistaLoginEmpleado.getNumeroBox();
+			if (numeroBox.matches("[0-9]+")) {
+				this.vistaPrincipalEmpleado = new VentanaMainEmpleado();
+				this.vistaPrincipalEmpleado.setActionListener(this);
+				this.vistaPrincipalEmpleado.setNumeroBox(numeroBox);
+		        this.vistaPrincipalEmpleado.setVisible(true);
+		        this.boxAtencion = new Box(Integer.parseInt(numeroBox));
+		        this.vistaLoginEmpleado.cerrarVentana();
+			} else
+				this.vistaLoginEmpleado.numeroBoxInvalido();
+		}
+		else if(e.getActionCommand().equals("LLAMAR CLIENTE")) {
+			Atencion atencion = new Atencion();
+			String proximoCliente = atencion.llamarCliente(this.boxAtencion);
+			if(proximoCliente.equals(""))
+				this.vistaPrincipalEmpleado.errorServidor();
+			else if(proximoCliente.equals("-1"))
+				this.vistaPrincipalEmpleado.noHayClienteParaAtender();
+			else
+				this.vistaPrincipalEmpleado.agregarCliente(proximoCliente);
+		}
+		
+	}
+
+	
+	public void run() {
+		this.vistaLoginEmpleado.setVisible(true);
+	}
 
 }
+
